1f7c979ea2084d31f811fee2c8a21b4848ad7a61
Aarow Epstein <aarowepstein1@gmail.com>
2023-06-13 17:53:01 -0700

    commit tree cdae95eef20c101d8328087e0147bb0f8c662014

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.include?(".")
   end
 
   def question?
-
+    self.include?("?")
   end
 
   def exclamation?
-
+    self.include?("!")
   end
 
   def count_sentences
-
+    split_sentence = self.split(/[?!.]/)
+    filter_sentence = split_sentence.filter {|i| i != ""}
+    filter_sentence.count
   end
 end
+
+
+
+
